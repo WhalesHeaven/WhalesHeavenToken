99f19811da32a6823a64b22ccb4c72a186f26d9d
Max Zab <max.zab87@gmail.com>
2020-06-16 13:45:35 +0300

    Bounty payout contract

---
+++ contracts/WhalesHeavenBountyPayout.sol
@@ -0,0 +1,39 @@
+pragma solidity ^0.5.0;
+
+import "openzeppelin-solidity/contracts/math/SafeMath.sol";
+import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
+
+
+interface IERC20 {
+    function totalSupply() external view returns (uint256);
+    function balanceOf(address account) external view returns (uint256);
+    function silentTransfer(address recipient, uint256 amount) external returns (bool);
+}
+
+
+contract WhalesHeavenBountyPayout is Ownable {
+
+  using SafeMath for uint256;
+
+  address public currencyAddress;
+
+  constructor(
+      address _currencyAddress
+  ) public {
+      currencyAddress = _currencyAddress;
+  }
+
+  function bountyDrop(address[] memory recipients, uint256[] memory values) onlyOwner public returns (bool) {
+      require(recipients.length == values.length);
+
+      for (uint256 i = 0; i < recipients.length; i++) {
+          token().silentTransfer(recipients[i], values[i]);
+      }
+
+      return true;
+  }
+
+  function token() public view returns (IERC20) {
+      return IERC20(currencyAddress);
+  }
+}
--- test/WhalesHeavenToken_spec.js
+++ test/WhalesHeavenToken_spec.js
@@ -1,13 +1,14 @@
 // /*global contract, config, it, assert*/
 const { assertRevert } = require('./helpers/assertRevert');
 const WhalesHeavenToken = artifacts.require("WhalesHeavenToken");
+const WhalesHeavenBountyPayout = artifacts.require("WhalesHeavenBountyPayout");
 
 const ETHER = 1000000000000000000;
+const ETHERSTRING ="000000000000000000";
 
 contract("WhalesHeavenToken", accounts => {
   beforeEach(async function () {
     this.owner = accounts[0];
-    this.party1 = accounts[1];
 
     this.WhalesHeavenToken = await WhalesHeavenToken.new({ from: this.owner });
   });
@@ -35,4 +36,94 @@ contract("WhalesHeavenToken", accounts => {
     assert.equal(creatorBalance, 50*1000000*ETHER);
   });
 
+  describe("Bounty Payout", function() {
+    beforeEach(async function () {
+      this.party1 = accounts[1];
+      this.party2 = accounts[2];
+      this.party3 = accounts[3];
+
+      this.amount1 = "235" + ETHERSTRING;
+      this.amount2 = "10000" + ETHERSTRING;
+      this.amount3 = "700" + ETHERSTRING;
+
+      this.WhalesHeavenBountyPayout = await WhalesHeavenBountyPayout.new(this.WhalesHeavenToken.address, { from: this.owner });
+    });
+
+    it('should return new vault address', async function () {
+      assert(/^0x([a-fA-F0-9]{40})$/.test(this.WhalesHeavenBountyPayout.address));
+    });
+
+    it('should make 0 party balance', async function () {
+      await this.WhalesHeavenToken.transfer(
+        this.WhalesHeavenBountyPayout.address,
+        "50000000" + ETHERSTRING,
+        { from: this.owner }
+      );
+
+      const party1Balance = await this.WhalesHeavenToken.balanceOf.call(this.party1);
+      const party2Balance = await this.WhalesHeavenToken.balanceOf.call(this.party2);
+      const party3Balance = await this.WhalesHeavenToken.balanceOf.call(this.party3);
+
+      assert.equal(party1Balance.toString(), "0");
+      assert.equal(party2Balance.toString(), "0");
+      assert.equal(party3Balance.toString(), "0");
+    });
+
+    it('should not make mass payout without balance', async function () {
+      await assertRevert(
+        this.WhalesHeavenBountyPayout.bountyDrop(
+          [this.party1, this.party2, this.party3],
+          [this.amount1, this.amount2, this.amount3],
+          { from: this.owner }
+        )
+      );
+    });
+
+    it('should make mass payout', async function () {
+      await this.WhalesHeavenToken.transfer(
+        this.WhalesHeavenBountyPayout.address,
+        "50000000" + ETHERSTRING,
+        { from: this.owner }
+      );
+
+      await this.WhalesHeavenBountyPayout.bountyDrop(
+        [this.party1, this.party2, this.party3],
+        [this.amount1, this.amount2, this.amount3],
+        { from: this.owner }
+      );
+
+      const contractBalance = await this.WhalesHeavenToken.balanceOf.call(this.WhalesHeavenBountyPayout.address);
+      assert.equal(contractBalance.toString(), "49989065" + ETHERSTRING);
+
+      const totalSuply = await this.WhalesHeavenToken.totalSupply.call();
+      assert.equal(totalSuply, 50*1000000*ETHER);
+
+      const party1Balance = await this.WhalesHeavenToken.balanceOf.call(this.party1);
+      const party2Balance = await this.WhalesHeavenToken.balanceOf.call(this.party2);
+      const party3Balance = await this.WhalesHeavenToken.balanceOf.call(this.party3);
+
+      assert.equal(party1Balance.toString(), "235" + ETHERSTRING);
+      assert.equal(party2Balance.toString(), "10000" + ETHERSTRING);
+      assert.equal(party3Balance.toString(), "700" + ETHERSTRING);
+    });
+
+
+    it('should not allow payout to not an owner', async function () {
+      this.party4 = accounts[4];
+
+      await this.WhalesHeavenToken.transfer(
+        this.WhalesHeavenBountyPayout.address,
+        "50000000" + ETHERSTRING,
+        { from: this.owner }
+      );
+
+      await assertRevert(
+        this.WhalesHeavenBountyPayout.bountyDrop(
+          [this.party1, this.party2, this.party3],
+          [this.amount1, this.amount2, this.amount3],
+          { from: this.party4 }
+        )
+      );
+    });
+  });
 })
